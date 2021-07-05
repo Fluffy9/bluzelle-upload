948ceabef1ec0fb2f0bf4f1974af0c13ac7e76f5
Fluffy9 <Fluffy9@users.noreply.github.com>
2021-07-05 19:07:44 -0400

    Update getBz.ts

--- getBz.ts
+++ getBz.ts
@@ -1,7 +1,6 @@
 import {bluzelle, BluzelleSdk} from "@bluzelle/sdk-js";
 import {memoize} from 'lodash'
 
-console.log(JSON.stringify(process.env))
 export const getBz = memoize(() => bluzelle({
    url: "https://client.sentry.testnet.private.bluzelle.com:26657",
    mnemonic: process.env['INPUT_MNEMONIC'],
