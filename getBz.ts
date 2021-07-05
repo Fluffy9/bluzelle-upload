a3cdd1f75392bc799292d89aa136094015bf2520
Fluffy9 <Fluffy9@users.noreply.github.com>
2021-07-05 18:46:19 -0400

    Use env variable

--- getBz.ts
+++ getBz.ts
@@ -3,7 +3,7 @@ import {memoize} from 'lodash'
 
 export const getBz = memoize(() => bluzelle({
    url: "https://client.sentry.testnet.private.bluzelle.com:26657",
-   mnemonic: "wink garment more rally beach before regret digital high like mandate blast labor happy few rib match valve state piece assist dentist trumpet dry",
+   mnemonic: process.env['mnemonic'],
     gasPrice: 0.002,
     maxGas: 1000000000
 }))
