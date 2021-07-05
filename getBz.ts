92a39601f0ed27ecfeab0b2ebb9ef6b928780ef8
Fluffy9 <Fluffy9@users.noreply.github.com>
2021-07-05 19:00:28 -0400

    Account for github variable naming

--- getBz.ts
+++ getBz.ts
@@ -3,7 +3,7 @@ import {memoize} from 'lodash'
 
 export const getBz = memoize(() => bluzelle({
    url: "https://client.sentry.testnet.private.bluzelle.com:26657",
-   mnemonic: process.env['mnemonic'],
+   mnemonic: process.env['INPUT_mnemonic'],
     gasPrice: 0.002,
     maxGas: 1000000000
 }))
